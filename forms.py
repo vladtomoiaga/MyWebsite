1ad0b8e79aaae28db8833a5d72250f67ea0afd07
Vlad Tomoiaga <vlad.tomoiaga@msg.group>
2025-10-23 08:01:38 +0300

    footer and header html deleted. Base html added. Forms changed

--- forms.py
+++ forms.py
@@ -9,6 +9,6 @@ class ContactForm(FlaskForm):
     subject = StringField(label="Subject",
                           validators=[Length(min=2, message="Field must be at least 2 characters long.")])
     message = TextAreaField(label="Message",
-                            validators=[Length(max=2000, message="Field must be maximum 2000 characters long.")])
+                            validators=[Length(min=2, max=2000, message="Message must between 2 and 2000 characters long.")])
     submit = SubmitField(label="Send Message")
     delete = SubmitField(label="Clear Form")
